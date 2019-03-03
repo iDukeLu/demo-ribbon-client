3dffd7fb8e3a0dcef2c6d3af31bf8c1c0865315f
Duke Lu <23153285987@qq.com>
2019-03-03 16:15:59 +0800

    主类初始化

--- src/main/java/com/idukelu/demo/demoribbon/Application.java
+++ src/main/java/com/idukelu/demo/demoribbon/Application.java
@@ -1,13 +1,14 @@
 package com.idukelu.demo.demoribbon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@SpringCloudApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 
 }
